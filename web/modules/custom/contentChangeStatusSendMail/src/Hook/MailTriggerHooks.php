bd93c8a0884e6908fa2efa1a89944d7479dc9b7c
sugi <p20prosugi@gmail.com>
2026-09-08 04:53:40 +0900

    fix: classベースの記法に置き換え。数年前の中途半端実装のままだったから細かく実装していく

---
+++ web/modules/custom/contentChangeStatusSendMail/src/Hook/MailTriggerHooks.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\content_change_status_send_mail\Hook;
+
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+
+class MailTriggerHooks {
+  #[Hook('entity_update')]
+  public function entity_update(EntityInterface $entity) {
+    \Drupal::logger('custom_mail_module')->notice('yahho');
+    $mailManager = \Drupal::service('plugin.manager.mail');
+    $langcode = \Drupal::currentUser()->getPreferredLangcode();
+    $params['subject'] = 'さぶじぇくと';
+    $params['message'] = 'test送信';
+
+    $result = $mailManager->mail(
+      'content_change_status_send_mail',
+      'content_moderation_notification',
+      'example@example.com',
+      $langcode,
+      $params
+    );
+  }
+
+  #[Hook('mail')]
+  function send_mail($key, &$message, $params) {
+    $options = [
+      'langcode' => $message['langcode'],
+    ];
+    $message['to'] = 'p20prosugi@gmail.com';
+    $message['from'] = \Drupal::config('system.site')->get('mail');
+    $message['subject'] = t('@subject', ['@subject' => $params['subject']], $options);
+    $message['body'][] = $params['message'];
+  }
+}
+
