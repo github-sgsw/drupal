123ef751d3899fbbd429353d247b0c25a1d11994
sgsw <p20prosugi@gmail.com>
2026-09-10 17:24:11 +0900

    feat: 承認待ちワークフローメールの実装

--- web/modules/custom/contentChangeStatusSendMail/src/Hook/MailTriggerHooks.php
+++ web/modules/custom/contentChangeStatusSendMail/src/Hook/MailTriggerHooks.php
@@ -11,40 +11,74 @@ class MailTriggerHooks {
 
   #[Hook('node_update')]
   public function entityUpdate(NodeInterface $entity) {
+    $work_flow_machin_name = $this->getWorkflowMachinName($entity);
 
-    if ($work_flow_machin_name = $this->getWorkflowMachinName($entity) == 'draft') return;
+    if ($work_flow_machin_name == 'draft') return;
+
     /** @var MailManagerInterface $mailManager */
     $mail_manager = \Drupal::service('plugin.manager.mail');
     $langcode = \Drupal::currentUser()->getPreferredLangcode();
 
-    match ($work_flow_machin_name) {
-      'published' => $this->rejectMail($entity, $mail_manager),
-      'unpublished' => $this->rejectMail($entity, $mail_manager),
-      'pending_approval' => $this->rejectMail($entity, $mail_manager),
-      'reject' => $this->rejectMail($entity, $mail_manager),
-      default => $this->rejectMail($entity, $mail_manager)
-    };
-  }
-
-  private function rejectMail(NodeInterface $entity, MailManagerInterface $mail_manager) {
     $params['subject'] = $entity->label();
     $params['message'] = <<<TEXT
       {$this->getWorkflowDispName($entity)}
 
-      {$entity->getRevisionUser()->getDisplayName()} から、ワークフローが差し戻されました。
+      {$entity->getRevisionUser()->getDisplayName()} から、ワークフローが{$this->getWorkflowDispName($entity)}になりました。
       内容の確認をお願いします。
 
       対象コンテンツ編集画面
       {$entity->toUrl('edit-form', ['absolute' => TRUE])->toString()}
 
       メッセージ
-      ここにリビジョンメッセージ乗せる
+      {$entity->getRevisionLogMessage()}
     TEXT;
 
+    match ($work_flow_machin_name) {
+      'published' => $this->rejectMail($entity, $mail_manager, $params),
+      'unpublished' => $this->rejectMail($entity, $mail_manager, $params),
+      'pending_approval' => $this->pendingApprovalMail($entity, $mail_manager, $params),
+      'reject' => $this->rejectMail($entity, $mail_manager, $params),
+      default => $this->rejectMail($entity, $mail_manager, $params)
+    };
+  }
+
+  private function rejectMail(NodeInterface $entity, MailManagerInterface $mail_manager, $params) {
     $mail_manager->mail(
       'content_change_status_send_mail',
       'content_moderation_notification',
       $this->getPreviousRevisionNode($entity)->getRevisionUser()->getEmail(),
+      'ja',
+      $params
+    );
+  }
+
+  /**
+   * コンテンツの公開権限を持つユーザーのみに承認待ちメールを送付する
+   */
+  private function pendingApprovalMail(NodeInterface $entity, MailManagerInterface $mail_manager, $params) {
+    $roles = \Drupal::entityTypeManager()->getStorage('user_role')->loadMultiple();
+    $target_permission = 'use default transition publish';
+
+    $matching_roles = array_filter($roles, function ($role) use ($target_permission) {
+      /** @var \Drupal\user\RoleInterface $role */
+      return $role->hasPermission($target_permission);
+    });
+
+    $role_ids = array_keys($matching_roles);
+
+    $uids = \Drupal::entityTypeManager()->getStorage('user')
+      ->getQuery()
+      ->accessCheck()
+      ->condition('status', 1)
+      ->condition('roles', array_values($role_ids), 'IN')
+      ->execute();
+
+    $users = \Drupal::entityTypeManager()->getStorage('user')->loadMultiple($uids);
+
+    $mail_manager->mail(
+      'content_change_status_send_mail',
+      'content_moderation_notification',
+      implode(',', array_map(fn($user) => $user->getEmail(), $users)),
       'ja',
       $params
     );
